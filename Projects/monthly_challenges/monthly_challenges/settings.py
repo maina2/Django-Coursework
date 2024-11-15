9208def7c6024a91a9f35fccd0888d0a908c29b1
Simon Chanzu <chanzusimon6@gmail.com>
2024-11-15 22:05:11 +0300

    uploads today

--- Projects/monthly_challenges/monthly_challenges/settings.py
+++ Projects/monthly_challenges/monthly_challenges/settings.py
@@ -56,9 +56,11 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             BASE_DIR / "challenges"/ "templates",
-            BASE_DIR / "templates"
+            BASE_DIR / "templates",
         ],
+        
         'APP_DIRS': True,
+        
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -119,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
