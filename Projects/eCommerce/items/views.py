bc7df848c6024c2dd80bb2ff7c92b3f1ff0fdcd7
Simon Chanzu <chanzusimon6@gmail.com>
2024-11-20 18:45:53 +0300

    uploads today

--- Projects/eCommerce/items/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
-from .models import Product
-
-def product_list(request):
-    products = Product.objects.all()[:20]  # Get the first 20 items
-    return render(request, 'product_list.html', {'products': products})
