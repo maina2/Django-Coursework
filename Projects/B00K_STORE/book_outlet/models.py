2240f8277eb286214c2443dad5dd2d9359f8c1d8
Simon Chanzu <chanzusimon6@gmail.com>
2024-11-25 12:28:29 +0300

    uploads today

--- Projects/B00K_STORE/book_outlet/models.py
+++ Projects/B00K_STORE/book_outlet/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField()
+    title = models.CharField(max_length=200)  # Add title field
+    author = models.CharField(max_length=100)  # Add author field
+    is_bestseller = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title} ({self.rating})"
--- Projects/B00K_STORE/book_outlet/views.py
+++ Projects/B00K_STORE/book_outlet/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from book_outlet.models import Book
 # Create your views here.
+
+def index(request):
+    books = Book.objects.all()
+    return render(request, "book_outlet/index.html",{
+          "book": books,
+    })
+
+def book_details (request,id):
+    books = Book.objects.get(id=id)
+    return render(request,"book_outlet/book-detail.html",{
+        "title": books.title,
+        "rating":books.rating,
+        "is_bestseller":books.is_bestseller,
+        "author":books.author
+
+
+    })
