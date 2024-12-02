c8dfcd391f92861e371a9116b44efa929dcdf59b
Simon Chanzu <chanzusimon6@gmail.com>
2024-12-02 20:07:50 +0300

    uploads today

--- Projects/feedback/reviews/forms.py
+++ Projects/feedback/reviews/forms.py
@@ -4,3 +4,6 @@ class ReviewForm(forms.Form):
     user_name = forms.CharField(label="Your name",required=True, error_messages={
         "required":"Your name is required!"
     })
+    texfield= forms.CharField(widget=forms.Textarea, label="Your feedback")
+    review = forms.IntegerField(max_value=5,min_value=1)
+    
