fe704acaa7a366881f5f83c8880988c982e17b6f
Simon Chanzu <chanzusimon6@gmail.com>
2024-12-09 13:15:12 +0300

    uploads

--- Projects/my_site/blog/models.py
+++ Projects/my_site/blog/models.py
@@ -18,7 +18,7 @@ class Author(models.Model):
 class Post(models.Model):
     title= models.CharField(max_length=150)
     excerpt= models.CharField(max_length=200)
-    image_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="posts", null=True)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True, max_length=100, blank=True)  # Allow blank for auto-generation
     content = models.TextField()
